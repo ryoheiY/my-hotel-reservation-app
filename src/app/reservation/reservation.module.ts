4771bfb2fad07d8e2f88254ba87250aa47e2bc58
ryoheiY <momoyamashia@icloud.com>
2024-05-19 21:09:55 +0900

    Styleを修正！

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -4,18 +4,21 @@ import { ReservationFormComponent } from '../reservation-form/reservation-form.c
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HomeModule } from '../home/home.module';
+
 
 
 @NgModule({
   declarations: [
     ReservationFormComponent,
-    ReservationListComponent
+    ReservationListComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    HomeModule
   ]
 })
 export class ReservationModule { }
